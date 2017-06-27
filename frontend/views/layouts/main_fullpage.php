45cc8a3f378c4e4402a461aa2b6068de78ca33f4
zengkai <kai.zeng@bookgoal.com.cn>
2017-06-27 14:17:41 +0800

    a
    Signed-off-by: zengkai <kai.zeng@bookgoal.com.cn>

--- frontend/views/layouts/main_fullpage.php
+++ frontend/views/layouts/main_fullpage.php
@@ -69,11 +69,9 @@ AppAsset::register($this);
     ]);
     NavBar::end();
     ?>
-
-    <div class="container">
-        <?= $content ?>
-    </div>
 </div>
+
+    <?= $content ?>
 
 <?php $this->endBody() ?>
 </body>
