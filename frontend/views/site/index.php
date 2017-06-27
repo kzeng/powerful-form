dd6aaccfdfdf81ef859b02174b7bdb78c8f2390e
zengkai <kai.zeng@bookgoal.com.cn>
2017-06-27 15:19:30 +0800

    a
    Signed-off-by: zengkai <kai.zeng@bookgoal.com.cn>

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -11,9 +11,9 @@ $this->title = '超级表单';
         
         <div class="jumbotron">
 
-            <h2>人人可用的在线表单工具</h2>
+            <h2>超级表单</h2>
 
-            <p class="lead">帮你收集和管理工作中的数据，易如反掌。</p>
+            <p class="lead">帮你收集和管理工作中的数据，人人可用，易如反掌。</p>
 
             <p>
                 <a href="http://pf.mitoto.cn/admin" class="btn btn-lg btn-success">现在就去免费创建一个表单</a>
