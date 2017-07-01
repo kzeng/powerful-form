6fd68ba7a7de1412c3e7e62c95eaac9ce3bf0b77
zengkai <kai.zeng@bookgoal.com.cn>
2017-07-01 09:37:46 +0800

    a
    Signed-off-by: zengkai <kai.zeng@bookgoal.com.cn>

--- backend/config/main.php
+++ backend/config/main.php
@@ -8,6 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-backend',
+    'homeUrl' => '/admin',
     'basePath' => dirname(__DIR__),
    
     'controllerNamespace' => 'backend\controllers',
