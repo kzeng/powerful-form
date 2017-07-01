94cfb42704d122d296476c0452a21ea450cf7a97
zengkai <kai.zeng@bookgoal.com.cn>
2017-07-01 10:23:23 +0800

    a
    Signed-off-by: zengkai <kai.zeng@bookgoal.com.cn>

--- backend/config/main.php
+++ backend/config/main.php
@@ -51,14 +51,14 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+        
     ],
     'params' => $params,
 ];
