c14c35a9a9445406727f1955987fe3630706a326
zengkai <kai.zeng@bookgoal.com.cn>
2017-07-07 09:36:42 +0800

    a
    Signed-off-by: zengkai <kai.zeng@bookgoal.com.cn>

--- backend/views/pform/_form.php
+++ backend/views/pform/_form.php
@@ -53,12 +53,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'detail')->widget(TinyMce::className(), [
         'clientOptions' => [
             'language' => 'zh_CN',
-            'plugins' => [
-                "advlist autolink lists link charmap print preview anchor",
-                "searchreplace visualblocks code fullscreen",
-                "insertdatetime media table contextmenu paste"
-            ],
-            'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+             'mode' => "textareas", 
         ]
     ]); ?>
 
