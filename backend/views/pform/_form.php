1f38b4713d1ff0de4fd4180ce588cf62d2969344
zengkai <kai.zeng@bookgoal.com.cn>
2017-07-07 14:16:13 +0800

    a
    Signed-off-by: zengkai <kai.zeng@bookgoal.com.cn>

--- backend/views/pform/_form.php
+++ backend/views/pform/_form.php
@@ -61,9 +61,9 @@ use yii\widgets\ActiveForm;
             //'image_dimensions' => true,
             //'image_prepend_url' => 'http://127.0.0.1/yii2-app-kit/backend/web',
             'image_prepend_url' => Yii::getAlias('@backend'),
-            
+
             'plugins' => [
-                'advlist autolink lists link image media emoticons template textcolor charmap print preview anchor searchreplace visualblocks code contextmenu table imagetools',
+                'advlist autolink lists link image media emoticons template textcolor colorpicker charmap print preview anchor searchreplace visualblocks code contextmenu table imagetools',
             ],
             'toolbar' => 'undo redo | styleselect |forecolor backcolor emoticons| bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media| code',
         ],
