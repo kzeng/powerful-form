c4d3555471adef068b0dc570991de26830ce893b
zengkai <zengkai001@qq.com>
2017-07-07 22:25:42 +0800

    a
    Signed-off-by: zengkai <zengkai001@qq.com>

--- backend/config/main.php
+++ backend/config/main.php
@@ -21,7 +21,37 @@ return [
             'uploadUrl' => '@web/uploads',
             'imageAllowExtensions'=>['jpg','png','gif']
         ],
+
+        'filemanager' => [
+        'class' => 'pendalf89\filemanager\Module',
+        // Upload routes
+        'routes' => [
+            // Base absolute path to web directory
+            'baseUrl' => '',
+            // Base web directory url
+            'basePath' => '@frontend/web',
+            // Path for uploaded files in web directory
+            'uploadPath' => 'uploads',
+        ],
+        // Thumbnails info
+        'thumbs' => [
+            'small' => [
+                'name' => 'Мелкий',
+                'size' => [100, 100],
+            ],
+            'medium' => [
+                'name' => 'Средний',
+                'size' => [300, 200],
+            ],
+            'large' => [
+                'name' => 'Большой',
+                'size' => [500, 400],
+            ],
+        ],
+        ],
+
     ],
+
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
